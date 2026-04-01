def3af747ce150b3ba1f3e71d8890268db10b8ea
unknown <s1521021@gmail.com>
2026-04-01 21:36:40 +0900

    通知先タブの排他制御とUI配置の最適化

--- src/app/component/chat-window/chat-window.component.ts
+++ src/app/component/chat-window/chat-window.component.ts
@@ -109,7 +109,10 @@ export class ChatWindowComponent implements OnInit, OnDestroy, AfterViewInit {
 
   showTabSetting() {
     let coordinate = this.pointerDeviceService.pointers[0];
-    let option: PanelOption = { left: coordinate.x - 250, top: coordinate.y - 175, width: 500, height: 350 };
+    //チャットタブ設定の高さを少し変更
+    //let option: PanelOption = { left: coordinate.x - 250, top: coordinate.y - 175, width: 500, height: 350 };
+    let option: PanelOption = { left: coordinate.x - 250, top: coordinate.y - 175, width: 500, height: 390 };
+    
     let component = this.panelService.open<ChatTabSettingComponent>(ChatTabSettingComponent, option);
     component.selectedTab = this.chatTab;
   }
